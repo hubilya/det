20fd798593f6231c58effccc4f50d3a13ffee6a4
Suffer Engineer <57523413+hubilya@users.noreply.github.com>
2025-01-10 18:27:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// window.onload = function () {
-//     document.getElementById("video1").addEventListener("ended", function () {
-//     document.getElementById("video2").play();
-//     document.getElementById("video2").classList.remove("hide");
-//     document.getElementById("video1").classList.add("hide")
+window.onload = function () {
+    document.getElementById("video1").addEventListener("ended", function () {
+    document.getElementById("video2").play();
+    document.getElementById("video2").classList.remove("hide");
+    document.getElementById("video1").classList.add("hide")
   
-//   });
-//     document.getElementById("video2").addEventListener("ended", function () {
-//     document.getElementById("video1").play();
-//     document.getElementById("video1").classList.remove("hide");
-//     document.getElementById("video2").classList.add("hide")
+  });
+    document.getElementById("video2").addEventListener("ended", function () {
+    document.getElementById("video1").play();
+    document.getElementById("video1").classList.remove("hide");
+    document.getElementById("video2").classList.add("hide")
   
-//   });
+  });
 
-//     }
+    }
