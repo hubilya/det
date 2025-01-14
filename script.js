2ee116072916ec2a7a3a6fdb13e12182927c2cea
Suffer Engineer <57523413+hubilya@users.noreply.github.com>
2025-01-14 21:01:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,87 @@
-// window.onload = function () {
-//     document.getElementById("video1").addEventListener("ended", function () {
-//     document.getElementById("video2").play();
-//     document.getElementById("video2").classList.remove("hide");
-//     document.getElementById("video1").classList.add("hide")
+// // window.onload = function () {
+// //     document.getElementById("video1").addEventListener("ended", function () {
+// //     document.getElementById("video2").play();
+// //     document.getElementById("video2").classList.remove("hide");
+// //     document.getElementById("video1").classList.add("hide")
   
-//   });
-//     document.getElementById("video2").addEventListener("ended", function () {
-//     document.getElementById("video1").play();
-//     document.getElementById("video1").classList.remove("hide");
-//     document.getElementById("video2").classList.add("hide")
+// //   });
+// //     document.getElementById("video2").addEventListener("ended", function () {
+// //     document.getElementById("video1").play();
+// //     document.getElementById("video1").classList.remove("hide");
+// //     document.getElementById("video2").classList.add("hide")
   
-//   });
+// //   });
 
-//     }
+// //     }
+
+
+//         // <div class="vancontainer" id="">
+//         //     <div class="van" id="van"><img src="minivan.png">
+//         //     </div>
+//         //     <button onclick="start()">ORDER NOW!</button>
+//         // </div> 
+        
+// /* .vancontainer{
+//     height:150px;
+//     width:100%;
+    
+//     position:relative;
+//     overflow:hidden;
+//     display:flex;
+//    background:#e4e4e4;
+//     justify-content:center;
+//   }
+//   .van{ position:absolute;width:200px;height:200px;;
+//   }
+//   .van img{
+//     width:100%;
+//     height:100%; 
+//   }
+  
+//   .vangoes {
+//     animation-name: up;
+//     animation-duration: 3s;
+//     animation-fill-mode: forwards;
+//    position:absolute;
+    
+//   }
+//   .vancontainer button{
+//     background:rgba(255, 255, 255, 0.3);
+//     color:#070707;
+//     border-style: none;
+//     width:100px;
+//     height:20px;
+//     position:absolute;
+//   }
+
+
+
+
+//   @keyframes up {
+//     0%   { transform: translateX(0px); }
+    
+//     100% { transform: translatex(1700px);
+//    }
+//   } */
+// // function start(){ 
+// //     document.querySelector('#van').classList.add('vangoes');
+// // }
+
+
+
+
+const observer = new IntersectionObserver(intersections => {
+  intersections.forEach(({
+  target,
+  isIntersecting
+  }) => {
+  target.classList.toggle('animate', isIntersecting);
+  });
+  }, {
+  threshold: 0
+  });
+  
+  document.querySelectorAll('#ok').forEach(div => {
+  observer.observe(div);
+  });
+
